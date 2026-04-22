45588cd155e26dbbc40b002384fd31b7f239b7ed
Shishir <shishir98@live.com>
2026-04-23 03:21:42 +0530

    Install feature for phones

--- budget_project/urls.py
+++ budget_project/urls.py
@@ -3,19 +3,27 @@ from django.urls import path, include
 from django.conf import settings
 from django.http import FileResponse, Http404
 from pathlib import Path
+import mimetypes
 
 STATIC_ROOT = settings.BASE_DIR / 'static'
 
-def serve_root_asset(request, filename, content_type):
+mimetypes.init()
+
+def serve_root_asset(request, filename):
     filepath = STATIC_ROOT / filename
     if not filepath.exists():
         raise Http404()
+    content_type, _ = mimetypes.guess_type(str(filepath))
+    if not content_type:
+        content_type = 'application/octet-stream'
     return FileResponse(open(filepath, 'rb'), content_type=content_type)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sw.js', lambda request: serve_root_asset(request, 'sw.js', 'application/javascript')),
-    path('manifest.json', lambda request: serve_root_asset(request, 'manifest.json', 'application/manifest+json')),
-    path('favicon.ico', lambda request: serve_root_asset(request, 'icons/icon-192.png', 'image/png')),
+    path('sw.js', lambda request: serve_root_asset(request, 'sw.js')),
+    path('manifest.json', lambda request: serve_root_asset(request, 'manifest.json')),
+    path('favicon.ico', lambda request: serve_root_asset(request, 'icons/icon-192.png')),
+    path('js/<path:filename>', lambda request, filename: serve_root_asset(request, f'js/{filename}')),
+    path('icons/<path:filename>', lambda request, filename: serve_root_asset(request, f'icons/{filename}')),
     path('', include('core.urls')),
 ]
